b73259650c55c24c2c9bd13fb30c26b1028db3bb
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-09-30 12:22:31 +0530

    new

---
+++ for loop/7_prime.c
@@ -0,0 +1,40 @@
+// check if the number is prime or not
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter a number to check if prime: ");
+    scanf("%d",&n);
+
+    int is_prime = 1;
+
+    if(n<=1){
+        printf("%d is not prime. ",n);
+    }
+    else
+    {
+        for(int i=2; i*i<=n; i++)
+        {
+            if(n%i==0)
+            {
+                is_prime=0;
+                break;
+            } 
+
+        }
+
+    }
+    if(is_prime == 1)
+    {
+        printf(" %d is prime. ",n);
+    }
+    else
+    {
+        printf("%d is not prime. ",n);
+    }
+    
+    return 0;
+
+
+}
