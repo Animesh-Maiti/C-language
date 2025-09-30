e622201f1e04953d9428a9cf74d18c5758b8eea1
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-09-30 12:50:26 +0530

    new

---
+++ for loop/8_continue.c
@@ -0,0 +1,15 @@
+// use of continue statement.
+
+#include<stdio.h>
+int main()
+{
+    for(int i=0; i<=100; i++)
+    {
+        if(i%2==0)
+        {
+            continue;
+        }
+        printf(" %d ",i);
+    }
+    return 0;
+}
