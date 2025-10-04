43433617a498dc27009473a11b14ef1d82506660
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-04 21:10:00 +0530

    new

---
+++ for loop/11_count_digits.c
@@ -0,0 +1,20 @@
+// count the number of digits in  a given number
+
+#include<stdio.h>
+int main()
+{
+    int num,temp,digit, count=0;
+    printf("enter the number: ");
+    scanf("%d",&num);
+
+    temp = num;
+    while(temp>0)
+    {
+        count++;
+        temp=temp/10;
+
+    }
+
+    printf("\nthe number of digit = %d ",count);
+    return 0;
+}
