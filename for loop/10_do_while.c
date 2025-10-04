e65d57f43d3648a9b97865a5ce9db345936257e8
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-04 20:38:09 +0530

    new

---
+++ for loop/10_do_while.c
@@ -0,0 +1,14 @@
+// do while loop 
+
+#include<stdio.h>
+int main()
+{
+    int i =10;
+    do{
+        printf("hello");
+    }while(i>11);
+
+    // we can see atleast one time the code runs before even it checks for the condition
+
+    return 0;
+}
