35b15e2d9a6148eab8d66d7887a4308cbe783071
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-05 13:29:00 +0530

    new

---
+++ for loop/15_series.c
@@ -0,0 +1,27 @@
+// find the sum of the series upto n. (1-2+3-4+5-6....n)
+
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    printf("enter the number n: ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            sum=sum-i;
+        }
+        else
+        {
+            sum=sum+i;
+        }
+    }
+
+
+    printf("the sum of the series is %d",sum);
+
+
+    return 0;
+}
