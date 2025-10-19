dba57b39d8316db2954d057a34f8500562573f1a
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-19 21:38:59 +0530

    new

---
+++ patterns/pattern_2.c
@@ -0,0 +1,17 @@
+// pattern 2
+
+#include<stdio.h>
+int main()
+{
+    for(int i=0; i<4; i++)
+    {
+        for(int j=1; j<5; j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+}
