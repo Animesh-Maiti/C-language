3bb4c3ae498f15bd6940be8e26c11e55ef895944
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-19 21:20:38 +0530

    new

---
+++ patterns/pattern_1.c
@@ -0,0 +1,19 @@
+// print the pattern 1.
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number of rows: ");
+    scanf("%d",&num);
+
+    for(int i=0; i<num; i++)
+    {
+        for (int j=0; j<=4; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
