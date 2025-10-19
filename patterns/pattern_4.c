d98c2aa5baebec3abad21f127fd565b344ea19e3
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-19 21:52:17 +0530

    new

---
+++ patterns/pattern_4.c
@@ -0,0 +1,17 @@
+// pattern 4
+
+#include<stdio.h>
+int main()
+{
+    for( int i=4; i>=1; i--)
+    {
+        for(int j=0; j<i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+}
