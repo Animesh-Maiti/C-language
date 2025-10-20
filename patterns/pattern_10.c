c2108c2fdba56b7e21b7ed6b48d0282478ea1715
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-20 21:09:47 +0530

    new

---
+++ patterns/pattern_10.c
@@ -0,0 +1,29 @@
+// pattern_10....hollow rectangle
+
+#include<stdio.h>
+int main()
+{
+    int rows, cols;
+    printf("Enter the number of rows: ");
+    scanf("%d",&rows);
+    printf("Enter the number of ccols: ");
+    scanf("%d",&cols);
+
+    for(int i=0; i<rows; i++)
+    {
+        for(int j=0; j<cols; j++)
+        {
+            if(i==0 || j==0 || i==rows-1 || j==cols-1)
+            {
+
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
