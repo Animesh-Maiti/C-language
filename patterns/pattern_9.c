b2a33b50bd4bfd347bafd5a209f14a94de75b17a
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-20 13:15:09 +0530

    new

---
+++ patterns/pattern_9.c
@@ -0,0 +1,29 @@
+// pattern 9 odd star plush...
+
+#include<stdio.h>
+int main()
+{
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d",&rows);
+
+    for(int i=1; i<=rows; i++)
+    {
+        for (int j=1; j<=rows; j++)
+        {
+            if(i==((rows+1)/2) || j== ((rows+1)/2))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+
+        }
+        printf("\n");
+        
+    }
+
+    return 0;
+}
