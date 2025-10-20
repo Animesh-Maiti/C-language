6084ae45adce8d1cc386546880b31d5fbc0f3d0a
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-20 12:56:29 +0530

    new

---
+++ patterns/pattern_8.c
@@ -0,0 +1,30 @@
+// pattern 8
+
+#include<stdio.h>
+int main()
+{
+    int rows;
+    printf("Enetr the number of rows: ");
+    scanf("%d",&rows);
+
+    for(int i=1; i<=rows; i++)
+    {
+        if(i%2!=0)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                 printf("%d ",j);
+            }
+        }
+        else
+        {
+            for(int j=1;j<=i;j++)
+            {
+                printf("%c ",(char)(j+64));
+            }
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
