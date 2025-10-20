b68ab80e669eaea11a897ac102adbd30ffcb61d3
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-20 12:05:36 +0530

    new

--- patterns/pattern_6.c
+++ patterns/pattern_6.c
@@ -17,6 +17,21 @@ int main()
         }
         printf("\n");
     }
+
+// second methord.
+
+    for(int i=1; i<=rows; i++)
+    {
+        int a=1;
+        for(int j=1;j<=i;j++)
+        {
+            
+            printf("%d ",a);
+            a+=2;
+            
+        }
+        printf("\n");
+    }
     return 0;
 }
 
