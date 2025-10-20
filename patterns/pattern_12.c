2b7c66afdd101c9eb59ef23b2d74fbdfc40c4281
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-20 22:14:16 +0530

    new

---
+++ patterns/pattern_12.c
@@ -0,0 +1,19 @@
+// pattern floyd's triangle....
+
+#include<stdio.h>
+int main()
+{
+    int rows, a=1;
+    printf("Enter the number of rows: ");
+    scanf("%d",&rows);
+
+    for(int i=0; i<rows; i++)
+    {
+        for(int j=0; j<=i; j++)
+        {
+            printf("%d ",a);
+            a++;
+        }
+        printf("\n");
+    }
+}
