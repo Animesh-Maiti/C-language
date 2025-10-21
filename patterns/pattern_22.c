861dfb10c071e2823e013a65b0f0bf7058dd8c82
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-22 01:08:59 +0530

    new

---
+++ patterns/pattern_22.c
@@ -0,0 +1,52 @@
+/*
+
+1234567
+123 567
+12   67
+1     7
+
+pattern 20 */
+
+#include<stdio.h>
+int main()
+{
+
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d",&rows);
+    int nsp=1;
+    int b=1;
+    for(int i=1; i<=(rows*2)-1; i++)
+    {
+        printf("%c ",(char)b+64);
+        b++;
+    }
+    printf("\n");
+
+    for(int i=1; i<rows; i++)
+    {
+        int a=1;
+        for(int j=rows-1 ; j>=i; j--)
+        {
+            printf("%c ",(char)a+64);
+            a++;
+        }
+
+        for(int k=nsp; k>=1; k-- )
+        {
+            printf("  ");
+            a++;
+        }
+
+        for(int j=rows-1 ; j>=i; j--)
+        {
+            printf("%c ",(char)a+64);
+            a++;
+        }
+
+        nsp+=2;
+        printf("\n");
+        
+    }
+    return 0;
+}
