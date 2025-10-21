c6532114af4bcdf15dc835a2c14915079322a0dd
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-21 10:59:17 +0530

    new

---
+++ patterns/pattern_13.c
@@ -0,0 +1,23 @@
+// pattern 13... 
+
+#include<stdio.h>
+int main()
+{
+    int rows;
+    printf("Emter the number of rows: ");
+    scanf("%d",&rows);
+
+    for(int i=1; i<=rows; i++)
+    {
+        for(int j=rows-1; j>=i; j--)
+        {
+            printf(" ");
+        }
+        for(int k=1; k<=i; k++ )
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
