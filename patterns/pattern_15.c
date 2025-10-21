ad61a2489749ba664b4834fc008fd144e3e18732
Animesh-Maiti <animeshmaiti862@gmail.com>
2025-10-21 11:20:53 +0530

    new

---
+++ patterns/pattern_15.c
@@ -0,0 +1,25 @@
+// pattern 15
+
+#include<stdio.h>
+int main()
+{
+    int rows;
+    printf("Entefrr the number of rows: ");
+    scanf("%d",&rows);
+
+    for(int i=1; i<=rows; i++)
+    {
+        for(int j=rows-1; j>=i; j--)
+        {
+            printf(" ");
+        }
+        int a=1;
+        for(int k=1; k<=i; k++)
+        {
+            printf("%c",(char)(a+64));
+            a++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
